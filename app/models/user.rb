b21dc01b5f8848ba98bcd09082836fa1e1d01848
iganao <naokiiga0504@gmail.com>
2021-08-13 06:57:02 +0900

    新規登録パスワード英数字　エラー文表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+  validates :nickname, presence: true, allow_blank: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, allow_blank: true }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, allow_blank: true }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :birthday, presence: true,allow_blank: true
+
+  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
 end
