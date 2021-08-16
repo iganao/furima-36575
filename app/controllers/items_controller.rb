22eb50e75c041ba11e0317971284d38e72fee190
iganao <naokiiga0504@gmail.com>
2021-08-16 15:08:02 +0900

    アクティブハッシュ、出品ページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def items_params
+  def item_params
     params.require(:items).permit(:image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :buy
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :postage_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :region_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shopping_date_id
+
+  validates :image, :product_name, :description, :category_id, :condition_id, :postage_id, :region_id, :shopping_date_id, :price, :user, presence: true
+
+  validates :category_id, :condition_id, :postage_id, :region_id, :shopping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :price, numericality: { less_than_or_equal_to: 9999999, 
+    greater_than_or_equal_to: 300 }, format: { with: /\A[0-9]+\z/ }
 end
